8bb7a2f00f8ff8fe8feadf1c22d2e2ae76e9fdd5
Lev Chelyadinov <leva181777@gmail.com>
2021-03-02 09:32:30 +0300

    Refactor the glass to use CSS variables

--- src/common/glass.svelte
+++ src/common/glass.svelte
@@ -12,27 +12,12 @@
   .glass {
     border-radius: 6px;
     display: flex;
-  }
+    background: var(--glass-bg);
+    box-shadow: var(--glass-shadow);
 
-  :global {
-    .theme_light .glass {
-      background: linear-gradient(180deg, rgba(244, 244, 244, 0.9) 0.82%, #E9E9E9 100%, rgba(234, 234, 234, 0) 100%);
-      box-shadow: inset -1px 1px 1px rgba(255, 255, 255, 0.5), inset 0px 0px 20px rgba(207, 207, 207, 0.5);
-
-      &.lit {
-        background: linear-gradient(180deg, #FFF2D1 0.82%, #FFD66C 100%);
-        box-shadow: inset -1px 1px 1px rgba(255, 255, 255, 0.5), inset 0px 0px 20px rgba(255, 176, 57, 0.8);
-      }
-    }
-
-    .theme_dark .glass {
-      background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 100%), radial-gradient(149.08% 93.3% at 38.75% 72.1%, #000000 0%, #231900 0.01%, #4D4D4D 100%);
-      box-shadow: inset -1px 1px 1px rgba(255, 255, 255, 0.2), inset 2px 2px 16px rgba(103, 103, 103, 0.6);
-
-      &.lit {
-        background: radial-gradient(91.67% 122.17% at 69.17% -11.17%, #FFA300 0%, #2D1C00 100%);
-        box-shadow: inset -1px 1px 1px rgba(255, 255, 255, 0.4), inset 0px 6px 15px rgba(255, 162, 0, 0.4);
-      }
+    &.lit {
+      background: var(--glass-bg-lit);
+      box-shadow: var(--glass-shadow-lit);
     }
   }
 </style>
