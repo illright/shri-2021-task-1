c90e92546c6bf59fc895a272219395b6fc57aa20
Lev Chelyadinov <leva181777@gmail.com>
2021-03-22 22:28:58 +0300

    Change the type definitions to match the 2nd task

--- src/data.d.ts
+++ src/data.d.ts
@@ -43,10 +43,12 @@ export interface VoteData {
 interface Period {
   /** заголовок периода */
   title: string;
+  /** название спринта */
+  hint?: string;
   /** число, значение для периода */
   value: number;
   /** является ли элемент текущим */
-  active: boolean;
+  active?: boolean;
 }
 
 /** Статистика, алиас шаблона `chart` */
